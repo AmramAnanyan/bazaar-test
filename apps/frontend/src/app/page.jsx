a8b5aca8ec4d0f436893410e3ac0005c2178dbe3
Amram1997 <amramananyan9@gmail.com>
2024-03-03 11:45:53 +0400

    first commit

--- apps/frontend/src/app/page.jsx
+++ apps/frontend/src/app/page.jsx
@@ -1,17 +1,16 @@
-'use client'
+'use client';
 
 import axios from 'axios';
 import React, { useCallback } from 'react';
 import { Button, Box } from '@mui/material';
 import styles from './index.module.scss';
 
-
 const Index = () => {
   const fetchAds = useCallback(() => {
     const fetchData = async () => {
       const { data } = await axios.get('/api/ads?minPrice=1000000');
       console.log(data);
-    }
+    };
     fetchData();
   }, []);
 
@@ -19,12 +18,12 @@ const Index = () => {
     <div className={styles.container}>
       <Box alignContent="center">
         <h1>And here it starts...</h1>
-        <Button onClick={fetchAds} variant='outlined'>
+        <Button onClick={fetchAds} variant="outlined">
           Send an API request
         </Button>
       </Box>
     </div>
   );
-}
+};
 
 export default Index;
